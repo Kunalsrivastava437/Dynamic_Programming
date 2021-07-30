992bb8394ecce516d5051d07af02b1caa84b60ba
Kunalsrivastava437 <kunalshrivastva437@gmail.com>
2021-07-30 23:51:04 +0530

    5 commit

--- DistinctSubsequences.java
+++ DistinctSubsequences.java
@@ -2,22 +2,24 @@ import java.util.*;
 public class DistinctSubsequences {
     private static int sol(String s, String t, int currS, int currT, int[][] dp){
 
-        int inc, exc = 0;
+        int inc=0, exc = 0;
 
         if(dp[currS][currT]!= -1){
             return dp[currS][currT];
         }
 
-        if(currS >= s.length && currT >= t.length){
+        if(currS >= s.length() && currT >= t.length()){
             return 1;
         }
 
         if(currS==currT){
-            int inc = sol(s, t, currS+1, currT+1,dp);
+            inc = sol(s, t, currS+1, currT+1,dp);
         }
-        
-        int exc = sol(s, t, currS+1, currT,dp);
-        return dp[currS][currT] = inc + exc;
+        else{
+        exc = sol(s, t, currS+1, currT,dp);
+        dp[currS][currT] = inc + exc;
+        }
+        return dp[currS][currT];
 
 
     }
@@ -25,11 +27,11 @@ public class DistinctSubsequences {
         String s= "rabbbit";
         String t = "rabbit";
         
-        int[][] dp = new int[7][6];
+        int[][] dp = new int[s.length()+1][t.length()+1];
         for(int[] a : dp){
             Arrays.fill(a,-1);
         }
-        int ans = sol(s, t, 6, 5,dp);
+        int ans = sol(s, t, s.length(), t.length(), dp);
         System.out.println(ans);
 
     }
---
+++ src/LCSThree.java
@@ -0,0 +1,46 @@
+package src;
+import java.util.Arrays;
+public class LCSThree {
+    public int lcs(String s1, String s2, String s3, int n, int m, int o, int[][][] dp){
+        if(n==0 || m==0 || o==0){
+            return 0;
+        }
+        
+        if(dp[n][m][o]!= -1){
+            return dp[n][m][o];
+        }
+        
+        if(s1.charAt(n - 1) == s2.charAt(m - 1)
+        && s1.charAt(n - 1)==s3.charAt(o - 1)){
+
+            return dp[n][m][o] = 1 + lcs(s1,s2,s3,n-1,m-1,o-1,dp);
+        }
+        else {
+            int c1 = lcs(s1,s2,s3,n,m-1,o,dp);
+            int c2 = lcs(s1,s2,s3,n-1,m,o,dp);
+            int c3 = lcs(s1,s2,s3,n,m,o-1,dp);
+            
+            dp[n][m][o] = Math.max(Math.max(c1,c2),c3);
+        }
+        return dp[n][m][o];
+    }
+    public static void main(String[] args) {
+
+        LCSThree l = new LCSThree();
+
+        String s1 = "abcde";
+        String s2 = "ace";
+        String s3 = "axcd";
+        int[][][] dp = new int[s1.length()+1][s2.length()+1][s3.length()+1];
+        
+        for(int[][] row : dp){
+            for(int[] column : row){
+
+            Arrays.fill(column,-1);
+            }
+        }
+        int ans = l.lcs(s1, s2, s3, s1.length(), s2.length(), s3.length(),dp);
+        System.out.println(ans);
+    }
+    
+}
